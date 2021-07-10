7a87f8ecce3141bcad003e2209de91190e5f9031
Mina <40896798+zRahmanfar@users.noreply.github.com>
2021-07-10 13:14:36 +0430

    Add files via upload

--- FristSpringBootApp/src/main/resources/templates/component/contactme.vue
+++ FristSpringBootApp/src/main/resources/templates/component/contactme.vue
@@ -1,5 +1,8 @@
 <template>
-    
+    <img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/>
+    <img src="https://img.icons8.com/fluent/48/000000/linkedin.png"/>
+    <img src="https://img.icons8.com/fluent/48/000000/whatsapp.png"/>
+    <img src="https://img.icons8.com/fluent/48/000000/gmail-new.png"/>
 </template>
 
 <script>
