79c5492732f3cda1eca65c89e0fcbff62de644ae
Mina <40896798+zRahmanfar@users.noreply.github.com>
2021-07-10 13:12:40 +0430

    Add files via upload

---
+++ contactme.vue
@@ -0,0 +1,16 @@
+<template>
+    <img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/>
+    <img src="https://img.icons8.com/fluent/48/000000/linkedin.png"/>
+    <img src="https://img.icons8.com/fluent/48/000000/whatsapp.png"/>
+    <img src="https://img.icons8.com/fluent/48/000000/gmail-new.png"/>
+</template>
+
+<script>
+    export default {
+        name: "contactme"
+    }
+</script>
+
+<style scoped>
+
+</style>
